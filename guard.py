fa5f58599384eaab541b5012338c5cd6ebc396e9
polomonk <275880829@qq.com>
2020-07-15 23:16:24 +0800

    更新了命名规范

--- guard.py
+++ guard.py
@@ -58,7 +58,7 @@ transform = transforms.Compose([
 ])
 
 rectangle_LinkedList = LinkedList()  # 创建一个链表存放最小矩形的信息
-last_centre_points = []  # 用来存放之前匹配的矩形中心点对
+before_target_points = []  # 用来存放之前匹配的矩形中心点对
 next_frame_coordinate = []  # 预测下一帧图像目标的坐标
 hit_coordinate = []  # 预测下n帧图像目标的坐标，即要打击的位置
 Track = 0  # 追踪标志，0丢失，1最优，2次优
@@ -78,10 +78,11 @@ def image_proprecessing():  # 提取图像中的颜色并2值化
     lower_bgr = np.array([150, 100, 150])  # bgr很亮部分的上下限
     upper_bgr = np.array([220, 220, 255])
     mask = cv2.inRange(img, lowerb=lower_bgr, upperb=upper_bgr)
+    mask = cv2.GaussianBlur(mask, (3, 3), 0)  # 平滑图像
     _, binary = cv2.threshold(mask, 200, 255, cv2.THRESH_BINARY)  # 限制亮度
 
-    binary = cv2.GaussianBlur(binary, (3, 3), 0)  # 平滑图像
-    _, binary = cv2.threshold(binary, 200, 255, cv2.THRESH_BINARY)  # 限制亮度
+    # binary = cv2.GaussianBlur(binary, (3, 3), 0)  # 平滑图像
+    # _, binary = cv2.threshold(binary, 200, 255, cv2.THRESH_BINARY)  # 限制亮度
 
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))  # 开操作，消除噪点
     binary = cv2.erode(binary, kernel)
@@ -96,12 +97,12 @@ def find_contours(img_binary):  # 找到所有轮廓的最小矩形
         # print(rect)   # point = rect[0]   side = rect[1]   angle = rect[2]
         if rect[1][0] * rect[1][1] >= 0:  # 面积太小的没有价值
             rectangle_LinkedList.append(np.int0(rect[0]), rect[1], rect[2])  # 把找到的轮廓存入链表
-    cur = rectangle_LinkedList.head
-    while cur is not None:
-        box = cv2.boxPoints((cur.point, cur.side, cur.orig_angle))
-        box = np.int0(box)  # 转换成整数操作
-        # cv2.drawContours(img, [box], -1, (0, 255, 0), 2)  # 画出其最小外接矩形
-        cur = cur.next
+    # cur = rectangle_LinkedList.head
+    # while cur is not None:
+    #     box = cv2.boxPoints((cur.point, cur.side, cur.orig_angle))
+    #     box = np.int0(box)  # 转换成整数操作
+    #     cv2.drawContours(img, [box], -1, (0, 255, 0), 2)  # 画出其最小外接矩形
+    #     cur = cur.next
 
 
 def image_intercept(point1, point2):  # 把图像ROI区域切割
@@ -113,14 +114,14 @@ def image_intercept(point1, point2):  # 把图像ROI区域切割
     return img[h_min - 100:h_max + 100, w_min - 200:w_max + 200]
 
 
-def area_p(std_area, comp_w, comp_h, error=20):  # 对面积进行限制
+def area_weight(std_area, comp_w, comp_h, error=20):  # 对面积进行限制
     if comp_h * (comp_w - error) < std_area < comp_h * (comp_w + error):
         return 1
     else:
         return 0
 
 
-def connect_p(std, comp, error=0.3):  # 对高度比限制
+def deviation_weight(std, comp, error=0.3):  # 对高度比限制
     if error < 0 or error > 1:
         return 0
     elif (1 - error) < std.height / comp.height < 1 / (1 - error):
@@ -139,7 +140,7 @@ def connect_p(std, comp, error=0.3):  # 对高度比限制
     return weight
 
 
-def angle_p(std, var, error=30):  # 2个矩形的角度差限制
+def angle_weight(std, var, error=30):  # 2个矩形的角度差限制
     if var - error < std < var + error:  # 角度差值不超过error
         probability = 1 - pow(abs(std - var) / error, 3)  # 用3次函数进行拟合，正态分布也行
         return probability
@@ -147,7 +148,7 @@ def angle_p(std, var, error=30):  # 2个矩形的角度差限制
         return -1
 
 
-def line_p(k, b, point, error=50):  # 用一个矩形的角度和中心点做直线，计算另一个点到直线的距离
+def parallel_weight(k, b, point, error=50):  # 用一个矩形的角度和中心点做直线，计算对应节点的中心点到直线的距离
     dist = abs(k * point[0] - point[1] + b) / np.sqrt(1 + k ** 2)
     if dist < error:  # 误差不超过error个像素点
         probability = 1 - pow(dist / error, 3)
@@ -156,23 +157,23 @@ def line_p(k, b, point, error=50):  # 用一个矩形的角度和中心点做直
         return -1
 
 
-def dist_p(centre_point, error=30):
+def continue_weight(centre_point, error=30):
     if Track == 0:
         return 0
     else:
         if Track is 1:
-            dist = distance(centre_point, last_centre_points[-1])
+            dist = distance(centre_point, before_target_points[-1])
             # print('distance', dist)
         elif sub_optimal_node is not None and Track is 2:
-            sub_centre_point = np.mean([sub_optimal_node.match_node.point, sub_optimal_node.point], axis=0),
-            dist = distance(sub_centre_point, centre_point)
+            sub_target_point = np.mean([sub_optimal_node.match_node.point, sub_optimal_node.point], axis=0),
+            dist = distance(sub_target_point, centre_point)
         else:
             return 0
         probability = 1 - pow(dist / error, 3)
         return probability
 
 
-def pred_p(centre_point, error=20):
+def prediction_weight(centre_point, error=20):
     deviation = distance(centre_point, next_frame_coordinate)
     if deviation > error:
         return 0
@@ -185,22 +186,19 @@ def judge_weight():  # 决策函数
     compare_obj = rectangle_LinkedList.head
     while compare_obj is not None:  # 2层循环把所有匹配情况都遍历一遍
         cur = rectangle_LinkedList.head
-        k = np.tan(np.radians(compare_obj.angle))  # 把角度转变成k
+        k = np.tan(np.radians(compare_obj.angle))  # 把角度转变成k斜率
         b = compare_obj.point[1] - k * compare_obj.point[0]  # 把中心点带入求出b
         while cur is not None:
             if cur is not compare_obj:  # 不允许自己匹配自己
                 weight = 0  # 权重
                 if 160 < cur.angle or cur.angle < 20:  # 角度再一定的区间内才计算权重
-                    # weight += 0.5*area_p(compare_obj.area, cur.weight, cur.height, error=30)        # 2个图像面积差距，辅助作用
-                    weight += 1 * connect_p(compare_obj, cur, error=0.3)  # 2个图像高度比值差，效果比面积好
-                    weight += 2 * angle_p(compare_obj.angle, cur.angle, error=15)  # 角度差距
-                    weight += 1.5 * line_p(k, b, cur.point, error=30)  # 点到直线的距离
+                    weight += 1 * deviation_weight(compare_obj, cur, error=0.3)  # 2个图像高度比值差，效果比面积好
+                    weight += 2 * angle_weight(compare_obj.angle, cur.angle, error=15)  # 角度差距
+                    weight += 1.5 * parallel_weight(k, b, cur.point, error=30)  # 点到直线的距离
                     # 可以增加高和匹配点的距离的关系
-                    if len(last_centre_points) > 0:  # 有迹可循
-                        cur_centre_point = np.mean([cur.point, compare_obj.point], axis=0)
-                        weight += 1 * pred_p(cur_centre_point,  # 预测下一帧点的位置与现在位置比较
-                                             error=abs((cur.height + compare_obj.height) // 4 + abs(
-                                                 cur.height - compare_obj.height)))  # 觉得预测比较准
+                    if len(before_target_points) > 0:  # 有迹可循
+                        cur_target_point = np.mean([cur.point, compare_obj.point], axis=0)
+                        weight += 1 * prediction_weight(cur_target_point, error=abs(cur.height + compare_obj.height) // 4) # 预测下一帧点的位置与现在位置比较
                     if compare_obj.weight < weight:  # 更新权重
                         compare_obj.weight = weight
                         compare_obj.match_node = cur
@@ -266,40 +264,36 @@ def show_result(cur):  # 结果可视化
 def refresh_state(optimal_node):  # 更新状态
     global Track
     if optimal_node.match_node is not None:  # 找到了权重最大的一对点
-        cur_centre_point = np.mean([optimal_node.point, optimal_node.match_node.point],
-                                   axis=0)  # .tolist()  # 当前一对点的中心位置
-        # last_point = last_centre_points[-1]  # 前一对点的中心位置
+        optim_target_point = np.mean([optimal_node.point, optimal_node.match_node.point], axis=0)   # 当前一对点的中心位置
+        # last_point = before_target_points[-1]  # 前一对点的中心位置
         reference_error = abs((optimal_node.height + optimal_node.match_node.height) // 4 + abs(
             optimal_node.height - optimal_node.match_node.height))  # 参考误差
-        if distance(next_frame_coordinate, cur_centre_point) < reference_error:  # 如果当前位置和预测位置差距不大则认为找到了目标
+        if distance(next_frame_coordinate, optim_target_point) < reference_error:  # 如果当前位置和预测位置差距不大则认为找到了目标
             Track = 1
             # cv2.circle(img, tuple(np.int0(hit_coordinate)), 3, color=(122, 45, 200), thickness=3)  # 预测目标的位置
         else:
             if sub_optimal_node is not None and sub_optimal_node.match_node is not None:  # 次优点与现在的位置差距也较大则认为目标丢失
-                sub_centre_point = np.mean([sub_optimal_node.point, sub_optimal_node.match_node.point], axis=0)  # 次优点的中心位置
-                reference_error = abs((sub_optimal_node.height + sub_optimal_node.match_node.height) // 2 - abs(
-                    sub_optimal_node.height - sub_optimal_node.match_node.height))  # 参考误差
-                if distance(sub_centre_point, cur_centre_point) < reference_error:
+                sub_target_point = np.mean([sub_optimal_node.point, sub_optimal_node.match_node.point], axis=0)  # 次优节点的中心位置
+                if distance(sub_target_point, optim_target_point) < reference_error:
                     Track = 2  # 继续追踪次优点
+                    before_target_points.clear()  # 之前收集的点没有意义了
+                    before_target_points.append(sub_optimal_node)  # 按顺序添加新的目标点
+                    before_target_points.append(optimal_node)
             else:  # 目标丢失
                 Track = 0
-
-            last_centre_points.clear()  # 之前收集的点没有意义了
+                before_target_points.clear()  # 之前收集的点没有意义了
 
 
 def find_optimal_node(low_limit=4):
     global sub_optimal_node
     sub_optimal_node = None
-    rectangle_LinkedList.sort_by_weight()  # 按权重从大到小排列
+    rectangle_LinkedList.sort_by_weight()  # 按权重从大到小排列desc
     head = rectangle_LinkedList.head
     if head is None or head.weight < low_limit:  # 如果开头就没东西就不用看后面了
         return None
 
     cur = rectangle_LinkedList.head
-    while cur is not None and cur.weight > low_limit:  # 保留权重较大的节点并画出来
-        # print(cur.point, cur.match_node.point, cur.area, cur.weight)
-        # cv2.circle(img, tuple(cur.point), 2, color=(255, 0, 0), )
-        # cv2.circle(img, tuple(cur.match_node.point), 2, color=(255, 0, 0), )
+    while cur is not None and cur.weight > low_limit:  # 保留权值超过阈值的
         cur = cur.next
     if cur is not None:  # 欲练神功，
         cur.next = None
@@ -318,7 +312,8 @@ def find_optimal_node(low_limit=4):
 
     if duplicate_matched_node:  # 如果有重复匹配的情况
         if duplicate_matched_node is not head.match_node and duplicate_matched_node is not head:  # 权重最大的节点可以确定就定为最优节点
-            optimal_node = duplicate_matched_node
+            optimal_node = head
+            sub_optimal_node = duplicate_matched_node
         else:  # 把被重复匹配的节点保留，其余节点榨干剩余价值后删除
             cur = rectangle_LinkedList.head
             while cur is not None:
@@ -341,9 +336,8 @@ def find_optimal_node(low_limit=4):
                     cur = cur.next
                 rectangle_LinkedList.remove(min_area_node)
 
-            head = rectangle_LinkedList.head  # 更新head
+            head = rectangle_LinkedList.head  # 更新节点匹配情况
             head.match_node = head.next
-            # print('处理后', head.point, head.match_node.point, head.area())
             optimal_node = head
     else:
         optimal_node = head
@@ -357,10 +351,10 @@ def find_optimal_node(low_limit=4):
 
 def main():
     global img
-    global next_frame_coordinate, hit_coordinate, last_centre_points, sub_optimal_node
+    global next_frame_coordinate, hit_coordinate, before_target_points, sub_optimal_node
     global left_up_point, right_down_point
     rectangle_LinkedList.clear()  # 清空链表开始操作新的帧
-    img_cp = img.copy()  # 后续操作会破坏原图，先备份
+    # img_cp = img.copy()  # 后续操作会破坏原图，先备份
     img_binary = image_proprecessing()  # 预处理，提取目标颜色，会改变源图
     find_contours(img_binary)  # 找到面积合适的最小矩形。用二值图找轮廓，画在原图上
     judge_weight()  # 进行权重判断
@@ -368,7 +362,7 @@ def main():
 
     if optimal_node is not None:  # 如果这帧图像拿到了最优节点
         if distance(optimal_node.point, optimal_node.match_node.point) is not 0:
-            # show_result(img, optimal_node)  # 展示结果
+            # show_result(optimal_node)  # 展示结果
             # 把匹配矩形转化成矩形的坐标点
             box1 = np.int0(cv2.boxPoints((optimal_node.point, optimal_node.side, optimal_node.orig_angle)).tolist())
             box2 = np.int0(cv2.boxPoints((optimal_node.match_node.point, optimal_node.match_node.side,
@@ -381,19 +375,17 @@ def main():
                 right_box = box1
             left_up_point = (min(left_box[0][0], left_box[1][0]), min(left_box[2][1], left_box[1][1], right_box[2][1]))
             right_down_point = (max(right_box[3][0], right_box[2][0]), max(left_box[0][1], right_box[0][1], right_box[3][1]))
-        # if optimal_node.match_node is not None:        # 有匹配的点对
-        last_centre_points.append(
-            np.mean([optimal_node.point, optimal_node.match_node.point], axis=0).tolist())  # 把最大权重点的中心坐标存入
+        # 把最大权重的点放进历史目标点
+        before_target_points.append(np.mean([optimal_node.point, optimal_node.match_node.point], axis=0).tolist())
+        if len(before_target_points) > 5:  # 用来预测的点数量，这里只保留4个
+            before_target_points.pop(0)
 
-        if len(last_centre_points) > 5:  # 用来预测的点数量，这里只保留4个
-            last_centre_points.pop(0)
-
-        next_frame_coordinate, hit_coordinate = kalman_filter(last_centre_points, prediction=3)  # 预测下一帧图和下n张图的坐标
+        next_frame_coordinate, hit_coordinate = kalman_filter(before_target_points, prediction=3)  # 预测下一帧图和下n张图的坐标
         refresh_state(optimal_node)  # 更新追踪的状态位
         if Track == 1:  # 追踪到了目标
             cv2.circle(img, tuple(np.int0(hit_coordinate)), 3, color=(122, 45, 200), thickness=3)  # 画出预测目标的位置
     else:
-        last_centre_points.clear()  # 这帧图像没有目标则清空之前保留的坐标点
+        before_target_points.clear()  # 这帧图像没有目标则清空之前保留的坐标点
     return img
 
 
@@ -418,11 +410,10 @@ if __name__ == '__main__':
     apd = 10
 
     video = r'D:\Document\py\opencv\demo\red_video\text11.avi'
-    img = cv2.imread(r'D:\Document\py\opencv\demo/blue_pic/500.jpg')
     cap = cv2.VideoCapture(video)
     ret, img = cap.read()
     while ret:
-        # st = time.time()
+        st = time.time()
 
         img = main()
         torch.no_grad()
@@ -431,7 +422,6 @@ if __name__ == '__main__':
         dst = Image.fromarray(dst).convert("RGB")
         data = transform(dst).unsqueeze(0)
         out = model(data.to(DEVICE)).to("cpu").item()
-        print(out)
         out = 0 if out < 0.5 else 1
         if out == 0:
             # cv2.rectangle(img, left_up_point, right_down_point, (0, 255, 0), 0)
@@ -439,8 +429,8 @@ if __name__ == '__main__':
         else:
             pass
         cv2.imshow('dst', img)
-        cv2.waitKey(0)
+        cv2.waitKey(30)
         ret, img = cap.read()
 
-        # et = time.time()
+        et = time.time()
         # print('time', et - st)
